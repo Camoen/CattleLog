4f0807bdea208f9eb2bf5d3fb764b24bad73ce00
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-09-08 19:02:05 -0400

    Add code to convert a .db file to a Room database

--- app/src/main/java/com/example/cattlelog/database/CattlelogDatabase.kt
+++ app/src/main/java/com/example/cattlelog/database/CattlelogDatabase.kt
@@ -3,13 +3,21 @@ package com.example.cattlelog.database
 import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
-
-const val DATABASE_NAME = "cattlelog_database"
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 public abstract class CattlelogDatabase : RoomDatabase() {
+
     // Singleton pattern to prevent multiple instances of DB being open concurrently
     // Credit: https://codelabs.developers.google.com/codelabs/android-room-with-a-view-kotlin/#6
     companion object {
+        const val DATABASE_NAME = "cattlelog_database"
+
+        val MIGRATION_1_to_2 : Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+            }
+        }
+
         @Volatile
         private var INSTANCE: CattlelogDatabase? = null
 
