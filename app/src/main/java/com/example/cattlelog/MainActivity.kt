c0410856d77c43e074fe640d0c2c2efb872db3ac
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-09-13 07:29:05 -0400

    Load pre-populated asset DB into Room using createFromAsset

--- app/src/main/java/com/example/cattlelog/MainActivity.kt
+++ app/src/main/java/com/example/cattlelog/MainActivity.kt
@@ -5,11 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import com.example.cattlelog.database.CattlelogDatabase
 import android.os.AsyncTask
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import com.example.cattlelog.entities.Cattle
-import com.example.cattlelog.utility.DatabaseToRoomConverter
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         AsyncTask.execute {
-            val allCattle = DatabaseToRoomConverter.getDatabase(this).roomDatabase.cattleDao().getAllCattle()
-            Log.d("MOO", allCattle.toString())
+            val allCattle = CattlelogDatabase.getDatabase(this).cattleDao().getAllCattle()
+            Log.d("TESTING123", allCattle.toString())
         }
     }
 }
--- app/src/main/java/com/example/cattlelog/database/CattlelogDatabase.kt
+++ app/src/main/java/com/example/cattlelog/database/CattlelogDatabase.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.cattlelog.dao.CattleDao
 import com.example.cattlelog.entities.Cattle
 
@@ -15,14 +13,8 @@ public abstract class CattlelogDatabase : RoomDatabase() {
     abstract fun cattleDao(): CattleDao
 
     // Singleton pattern to prevent multiple instances of DB being open concurrently
-    // Credit: https://codelabs.developers.google.com/codelabs/android-room-with-a-view-kotlin/#6
     companion object {
         const val DATABASE_NAME = "cattlelog_database"
-
-        val MIGRATION_1_to_2 : Migration = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-            }
-        }
 
         @Volatile
         private var INSTANCE: CattlelogDatabase? = null
@@ -32,12 +24,16 @@ public abstract class CattlelogDatabase : RoomDatabase() {
             if (tempInstance != null) {
                 return tempInstance
             }
+
             synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     CattlelogDatabase::class.java,
                     DATABASE_NAME
-                ).allowMainThreadQueries().build()
+                )
+                    .createFromAsset("databases/$DATABASE_NAME.db")
+                    .build()
+
                 INSTANCE = instance
                 return instance
             }
