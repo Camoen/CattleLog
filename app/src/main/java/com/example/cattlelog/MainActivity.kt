b3dd6181f5044b075a783a4befc6b92389ee05cf
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-09-21 14:53:19 -0400

    Clean up MainActivity. Use Room with downloader.

--- app/src/main/java/com/example/cattlelog/MainActivity.kt
+++ app/src/main/java/com/example/cattlelog/MainActivity.kt
@@ -4,10 +4,8 @@ import android.app.DownloadManager
 import android.content.Context
 import android.content.pm.PackageManager
 import android.Manifest
-import android.database.Cursor
-import android.database.DatabaseUtils
-import android.database.sqlite.SQLiteDatabase
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,20 +13,19 @@ import android.os.Environment
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import android.util.Log
+import com.example.cattlelog.database.CattlelogDatabase
 import java.io.*
 
 
 private const val PERMISSION_CODE = 1000
-
+private const val LOG_TAG = "outputs"
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         // Button Click Handler
         downloadButton.setOnClickListener {
@@ -68,23 +65,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun internalDownload(): Boolean {
-        return try {
-
-            true
-        } catch (e: Exception) {
-
-            false
-        }
-
-
-    }
-
     // Handles file download
     private fun startDownload() {
         val request = DownloadManager.Request(Uri.parse(getString(R.string.db_URL)))
         val time = System.currentTimeMillis()
-        Log.d("outputs", "time = $time")
+        Log.d(LOG_TAG, "time = $time")
         request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
         request.setTitle("cattlelog_database.db")
         request.setDescription("CattleLog database is downloading.")
@@ -95,75 +80,28 @@ class MainActivity : AppCompatActivity() {
         val manager =  getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
         manager.enqueue(request)
         val dbpath = Environment.getExternalStorageDirectory().toString()+ File.separator + Environment.DIRECTORY_DOWNLOADS + File.separator + "cattlelogdb_$time.db"
-        Log.d("outputs", "dbpath: $dbpath")
+        Log.d(LOG_TAG, "dbpath: $dbpath")
 
-        Log.d("outputs", "Entering installDatabaseFromDownloads")
+        Log.d(LOG_TAG, "Entering installDatabaseFromDownloads")
         // Copies database file into CattleLog's internal data
         installDatabaseFromDownloads(dbpath)
 
         // Sample database setup and query
         val dbFile = File(filesDir, "cattlelog_database.db")
-        Log.d("outputs", "dstFile: $dbFile")
-        val db = SQLiteDatabase.openOrCreateDatabase(dbFile, null)
-        Log.d("outputs", db.toString())
+        Log.d(LOG_TAG, "dstFile: $dbFile")
 
-        var c: Cursor
-        /* get cursor on it */
-        try
-        {
-            c = db.query("cattle", null,null, null, null, null, null);
-            Log.d("outputs"+"cursor", "cattle exists!")
-            c = db.query("treatment", null,null, null, null, null, null);
-            Log.d("outputs"+"cursor", "treatment exists!")
-            c = db.query("health", null,null, null, null, null, null);
-            Log.d("outputs"+"cursor", "health exists!")
-            c = db.query("userFields", null,null, null, null, null, null);
-            Log.d("outputs"+"cursor", "userFields exists!")
-            // set limit to 5 to dump first 5 records
-            c = db.query("cattle", null,null, null, null, null, null, "5");
-//            Log.v("outputs"+"cursor", DatabaseUtils.dumpCursorToString(c))
-//            while (c.moveToNext()) {
-//                Log.d("outputs", c.toString())
-//            }
-            c.close()
+        AsyncTask.execute {
+            Log.d(LOG_TAG, "" + CattlelogDatabase.getDatabase(applicationContext, dbFile).cattleDao().getAllCattle().size)
         }
-        catch (e: java.lang.Exception) {
-            Log.e("outputs", "exception", e)
-            Log.d("outputs"+"cursor", "Error in database setup or test query.")
-        }
-
-        Log.d("outputs", "Check if the database (created from the downloaded and copied .db file) has data.")
-        val tableName = "cattle"
-        val columnNames: Array<String> = arrayOf("TagNumber", "BirthDate", "FarmID")
-        val whereClause = "TagNumber < ?"
-        val whereArgs: Array<String> = arrayOf("750")
-        val groupBy = null
-        val having = null
-        val orderBy = null
-        val limit = "5"
-
-        try {
-            val c1: Cursor = db.query(tableName, columnNames, whereClause, whereArgs, groupBy, having, orderBy, limit)
-            Log.v("outputs" + "cursor", DatabaseUtils.dumpCursorToString(c1))
-            c1.close()
-        }
-        catch (e: java.lang.Exception) {
-            /* fail */
-            Log.e("outputs", "exception", e)
-            Log.d("outputs"+"cursor", "Cursor caused an exception.")
-        }
-        Log.d("outputs", "Database setup and query completed successfully.")
-
     }
 
-
     private fun installDatabaseFromDownloads(srcPath: String) {
-        Log.d("outputs", "We have not broken and dbPath: $srcPath")
+        Log.d(LOG_TAG, "We have not broken and dbPath: $srcPath")
         val source = File(srcPath)
 
         val startTime = System.currentTimeMillis()
         while (!source.exists() && System.currentTimeMillis()-startTime < 60000){
-            Log.d("outputs", "Waiting on file to download.")
+            Log.d(LOG_TAG, "Waiting on file to download.")
             Thread.sleep(1000)
         }
         // Ensure that the file is completely ready to be copied
@@ -173,23 +111,21 @@ class MainActivity : AppCompatActivity() {
         // Copy the downloaded file to CattleLog's internal storage.
         if (source.exists()){
             val destination = File(filesDir, "cattlelog_database.db")
-            Log.d("outputs", "We have not broken and source: $source")
-            Log.d("outputs", "We have not broken and destination: $destination")
+            Log.d(LOG_TAG, "We have not broken and source: $source")
+            Log.d(LOG_TAG, "We have not broken and destination: $destination")
 
             try {
                 File(source.toURI()).copyTo(File(destination.toURI()), overwrite = true)
-                Log.d("outputs", "File has been copied successfully.")
+                Log.d(LOG_TAG, "File has been copied successfully.")
             } catch (e: Exception) {
-                Log.d("outputs", "Error copying file.")
-                Log.e("outputs", "exception", e)
+                Log.d(LOG_TAG, "Error copying file.")
+                Log.e(LOG_TAG, "exception", e)
             }
 
         } else{
             // Handle case where file is not downloaded
-            Log.d("outputs", "Source file doesn't exist.")
+            Log.d(LOG_TAG, "Source file doesn't exist.")
         }
-
-
     }
 
     // TODO: Remove unneeded files from the Downloads folder if possible (not important)
@@ -198,20 +134,19 @@ class MainActivity : AppCompatActivity() {
         val root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
         val file = File("$root/cattlelog_database.db")
         val fdelete = File(file.toURI())
-        Log.d("outputs", "file: $file")
-        Log.d("outputs", "fdelete: $fdelete")
+        Log.d(LOG_TAG, "file: $file")
+        Log.d(LOG_TAG, "fdelete: $fdelete")
         val fileExists = fdelete.exists()
-        Log.d("outputs", fileExists.toString())
+        Log.d(LOG_TAG, fileExists.toString())
         if (fileExists) {
-            Log.d("outputs", "fileExists")
+            Log.d(LOG_TAG, "fileExists")
             if (fdelete.delete()) {
-                Log.d("outputs", "file Deleted :" + file.path)
+                Log.d(LOG_TAG, "file Deleted :" + file.path)
             } else {
-                Log.d("outputs", "file not Deleted :" + file.path)
+                Log.d(LOG_TAG, "file not Deleted :" + file.path)
             }
         }
     }
-
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -229,5 +164,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
 }
--- app/src/main/java/com/example/cattlelog/database/CattlelogDatabase.kt
+++ app/src/main/java/com/example/cattlelog/database/CattlelogDatabase.kt
@@ -1,6 +1,7 @@
 package com.example.cattlelog.database
 
 import android.content.Context
+import android.graphics.Typeface.createFromAsset
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -12,6 +13,7 @@ import com.example.cattlelog.entities.Cattle
 import com.example.cattlelog.entities.Health
 import com.example.cattlelog.entities.Treatment
 import com.example.cattlelog.entities.UserFields
+import java.io.File
 
 @Database(entities = [Cattle::class, Health::class, Treatment::class, UserFields::class], version = 1)
 public abstract class CattlelogDatabase : RoomDatabase() {
@@ -28,20 +30,28 @@ public abstract class CattlelogDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: CattlelogDatabase? = null
 
-        fun getDatabase(context: Context): CattlelogDatabase {
+        fun getDatabase(context: Context, databaseFile: File? = null): CattlelogDatabase {
             val tempInstance = INSTANCE
             if (tempInstance != null) {
                 return tempInstance
             }
 
             synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    CattlelogDatabase::class.java,
-                    DATABASE_NAME
-                )
-                    .createFromAsset("databases/$DATABASE_NAME.db")
-                    .build()
+                val instance: CattlelogDatabase
+
+                if (databaseFile !== null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        CattlelogDatabase::class.java,
+                        DATABASE_NAME
+                    ).createFromFile(databaseFile).build()
+                } else {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        CattlelogDatabase::class.java,
+                        DATABASE_NAME
+                    ).createFromAsset("databases/$DATABASE_NAME.db").build()
+                }
 
                 INSTANCE = instance
                 return instance
