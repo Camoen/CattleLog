fb8367fc06dde0912ff1ba97d772d14f50e3fd81
Camoen <camoenvw@gmail.com>
2019-09-28 14:31:16 -0400

    Add next expected heats report queries, query test button, comment out "onResume", update DB URL.

--- app/src/main/java/com/example/cattlelog/dao/CattleDao.kt
+++ app/src/main/java/com/example/cattlelog/dao/CattleDao.kt
@@ -11,4 +11,23 @@ interface CattleDao {
 
     @Query("SELECT * FROM cattle WHERE TagNumber = :tagNumber")
     fun getCattleWithTagNumber(tagNumber: Int): Cattle?
+//    fun getCattleWithTagNumber(tagNumber: Int): List<Cattle>?
+
+
+//    TODO: We can add custom intervals for next expected heats report.  This would take in a day range (3 days into past and 4 days into future, for example)
+    // Check the query below to see how it would be formulated.  I put FUTURE (imagine +4) and PAST (imagine 3) as placeholders for the formula required
+    // This is a BONUS feature, we can just use `getNextExpectedHeatsPreset()` for now.
+//    @Query("select * from cattle where DATE(NextExpHeat) BETWEEN DATE(datetime('now','localtime', '-PAST day')) AND DATE(datetime('now','localtime', 'FUTURE day'))\n" +
+//            "UNION\n" +
+//            "select * from cattle where DATE(NextExpHeat) BETWEEN DATE(datetime('now','localtime', '21-PAST day')) AND DATE(datetime('now','localtime', '21 day'));")
+//    fun getNextExpectedHeats(): Cattle?
+
+    // For Testing Only (returns a result)
+    @Query("select * from cattle where DATE(NextExpHeat) BETWEEN DATE(datetime('now','localtime', '-365 day')) AND DATE(datetime('now','localtime', '365 day'))")
+    fun getNextExpectedHeatsTEST(): Cattle?
+
+    @Query("select * from cattle where DATE(NextExpHeat) BETWEEN DATE(datetime('now','localtime', '-3 day')) AND DATE(datetime('now','localtime', '3 day'))\n" +
+            "UNION\n" +
+            "select * from cattle where DATE(NextExpHeat) BETWEEN DATE(datetime('now','localtime', '18 day')) AND DATE(datetime('now','localtime', '21 day'));")
+    fun getNextExpectedHeatsPreset(): Cattle?
 }
