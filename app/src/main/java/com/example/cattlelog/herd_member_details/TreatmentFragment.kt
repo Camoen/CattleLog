d95fbc5fb3b2532f2e516603c98629e4eb447692
Camoen <camoenvw@gmail.com>
2019-10-23 15:06:39 -0400

    Add TreatmentViewModelFactory. Show data in Treatments tab RecyclerView

--- app/src/main/java/com/example/cattlelog/herd_member_details/TreatmentFragment.kt
+++ app/src/main/java/com/example/cattlelog/herd_member_details/TreatmentFragment.kt
@@ -2,9 +2,7 @@ package com.example.cattlelog.herd_member_details
 
 
 import android.graphics.drawable.Drawable
-import android.os.AsyncTask
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,11 +14,10 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.cattlelog.R
 import com.example.cattlelog.herd.TreatmentListAdapter
 
-import com.example.cattlelog.model.database.CattlelogDatabase
 import kotlinx.android.synthetic.main.fragment_treatment.*
+import com.example.cattlelog.herd.TreatmentViewModelFactory
 
 
 /**
@@ -32,6 +29,7 @@ class TreatmentFragment : Fragment() {
     private var herdMemberTagNumber: Int = -1
     private var herdMemberBirthDate: String = ""
     private lateinit var treatmentViewModel: TreatmentViewModel
+    private lateinit var treatmentViewModelFactory: TreatmentViewModelFactory
     private lateinit var treatmentRecyclerView: RecyclerView
     private lateinit var treatmentAdapter: TreatmentListAdapter
 
@@ -39,30 +37,6 @@ class TreatmentFragment : Fragment() {
         super.onCreate(savedInstanceState)
         herdMemberTagNumber = (activity as HerdMemberDetails).getTagNumber()
         herdMemberBirthDate = (activity as HerdMemberDetails).getBirthDate()
-
-        // Use of "activity!!" is required to guarantee that "activity" will never be null. Calling with "activity" does not work.
-        AsyncTask.execute {
-            Log.d(
-                "TEST QUERY IN FRAGMENT",
-                "TREATMENT Test Query: " + CattlelogDatabase.getDatabase(activity!!).treatmentDao().getUniqueTreatmentData(
-                    herdMemberTagNumber, herdMemberBirthDate
-                )
-            )
-        }
-
-        treatmentAdapter = TreatmentListAdapter(activity!!)
-        treatmentRecyclerView = treatmentList
-        treatmentRecyclerView.layoutManager = LinearLayoutManager(activity!!)
-        treatmentRecyclerView.setHasFixedSize(true)
-        treatmentRecyclerView.adapter = treatmentAdapter
-        val divider = DividerItemDecoration(treatmentRecyclerView.context, DividerItemDecoration.VERTICAL)
-        divider.setDrawable(ContextCompat.getDrawable(activity!!, R.drawable.divider) as Drawable)
-        treatmentRecyclerView.addItemDecoration(divider)
-
-        treatmentViewModel = ViewModelProvider(this).get(TreatmentViewModel(activity!!.application,herdMemberTagNumber,herdMemberBirthDate)::class.java)
-        treatmentViewModel.allTreatments.observe(this, Observer { treatmentList ->
-            treatmentList?.let { treatmentAdapter.setTreatmentList(it) }
-        })
 
     }
 
@@ -72,6 +46,29 @@ class TreatmentFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(com.example.cattlelog.R.layout.fragment_treatment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        treatmentAdapter = TreatmentListAdapter(activity!!)
+        treatmentRecyclerView = treatmentList
+        treatmentRecyclerView.layoutManager = LinearLayoutManager(activity!!)
+        treatmentRecyclerView.setHasFixedSize(true)
+        treatmentRecyclerView.adapter = treatmentAdapter
+        val divider = DividerItemDecoration(treatmentRecyclerView.context, DividerItemDecoration.VERTICAL)
+        divider.setDrawable(ContextCompat.getDrawable(activity!!, com.example.cattlelog.R.drawable.divider) as Drawable)
+        treatmentRecyclerView.addItemDecoration(divider)
+
+
+        // treatmentViewModelFactory takes in extra parameters (TagNumber, BirthDate) and passes them to treatmentViewModel
+        treatmentViewModelFactory = TreatmentViewModelFactory(activity!!.application, herdMemberTagNumber, herdMemberBirthDate)
+        treatmentViewModel = ViewModelProvider(this, treatmentViewModelFactory).get(TreatmentViewModel::class.java)
+        treatmentViewModel.allTreatments.observe(this, Observer { treatmentList ->
+            treatmentList?.let { treatmentAdapter.setTreatmentList(it) }
+        })
+
+
     }
 
 
